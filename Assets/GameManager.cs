5b89790e4a4d78efe64e5f6844f27757ed524d30
TreeTree311 <103326654+TreeTree311@users.noreply.github.com>
2026-08-10 13:13:19 -0400

    gamemanager
    Started to implement a game manager script

---
+++ Assets/GameManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
